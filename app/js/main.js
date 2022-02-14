8f39e7e0654da36e720d34098887143c038ea0da
Nikita Sidiropulo <sidiropulonikita04@gmail.com>
2022-02-14 13:56:50 +0300

    сверстал шапку сайта

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+
+// ПЕРЕКЛЮЧЕНИЕ ТЕМЫ САЙТА
 const themeBtn = document.querySelector('.header__setting-theme');
 const body = document.querySelector('body');
 const darckNone = document.querySelectorAll('.darck-none');
@@ -7,3 +9,20 @@ themeBtn.addEventListener('click', function (e) {
 	body.classList.toggle('darck')
 	darckNone.classList.toggle("none")
 })
+
+// КНОПКА ВЫБОРА  ЯЗЫКА
+const langBtn = document.querySelector('.header__setting-lang');
+const langList = document.querySelector('.header__setting-lang-list');
+// const langEl = document.querySelectorAll('.header__setting-lang-el');
+
+langBtn.addEventListener('click', function (e) {
+	langList.classList.toggle('header__setting-lang-list_active')
+
+})
+
+let langEl = [...document.querySelectorAll('.header__setting-lang-el')].forEach(langEl => {
+	langEl.addEventListener('click', () => {
+		langBtn.textContent = langEl.textContent
+		langList.classList.remove('header__setting-lang-list_active')
+	});
+});
