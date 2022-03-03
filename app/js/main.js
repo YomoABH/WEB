81f4a995189eb72febed99818716b6087d23a9b3
Nikita Sidiropulo <sidiropulonikita04@gmail.com>
2022-03-03 15:01:16 +0300

    переверстал и адаптировал promo

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,45 @@
 
 // ПЕРЕКЛЮЧЕНИЕ ТЕМЫ САЙТА
+const mainVector = document.querySelector('.promo__img');
+const mainVectorMob = document.querySelector('.promo__img_mob');
+const logotip = document.querySelector('.header__logo-img');
 const body = document.querySelector('body');
+
 const themeBtn = [...document.querySelectorAll('.header__setting-theme')].forEach(themeBtn => {
 	themeBtn.addEventListener('click', () => {
-		themeBtn.classList.toggle('header__setting-theme_darck')
-		body.classList.toggle('darck')
+		themeBtn.classList.toggle('header__setting-theme_darck');
+		body.classList.toggle('darck');
+
+		// ЗАМЕНА КОНТЕНТА ГЛАВНОЙ SVG НА САЙТЕ
+		let dataStatus = mainVector.getAttribute('data'); //создаем переменную для хранения статуса data 
+
+		if (dataStatus === "img/bg/vectorSite.svg") {        			//если вектор для светлой темы
+			mainVector.setAttribute('data', 'img/bg/vectorSiteDarck.svg')//меняем на вектор тнмной темы
+		} else {  //если если вектор темной темы
+			mainVector.setAttribute('data', 'img/bg/vectorSite.svg') //меняем меняем на светлый вектор 
+		}
+		// ЗАМЕНА КОНТЕНТА ГЛАВНОЙ мобильной SVG НА САЙТЕ
+
+		let dataStatusMob = mainVectorMob.getAttribute('data'); //создаем переменную для хранения статуса data 
+
+		if (dataStatusMob === "img/bg/vectorSiteMob.svg") {        			//если вектор для светлой темы
+			mainVectorMob.setAttribute('data', 'img/bg/vectorSiteMobDark.svg')//меняем на вектор тнмной темы
+		} else {  //если если вектор темной темы
+			mainVectorMob.setAttribute('data', 'img/bg/vectorSiteMob.svg') //меняем меняем на светлый вектор 
+		}
+
+		//ЗАМЕНА ЛОГОТИПА ПРИ ТЕМНОЙ ТЕМЕ
+		let logoSrc = logotip.getAttribute('src'); //создаем переменную для хранения статуса src
+
+		if (logoSrc === "img/logo/main-logo.png") {        			//если логотип светлой темы
+			logotip.setAttribute('src', 'img/logo/logo-darck.png')//меняем на логотип темной темы
+		} else {  //если он темной темы
+			logotip.setAttribute('src', 'img/logo/main-logo.png') //меняем на светлую
+		}
+
 	});
 });
+
 
 // КНОПКА ВЫБОРА  ЯЗЫКА
 const langBtn = document.querySelector('.header__setting-lang');
@@ -41,13 +74,15 @@ const langEl2 = [...document.querySelectorAll('.header__setting-lang-el_2')].for
 // КНОПКА МЕНЮ БУРГЕРА
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
-const menuList = document.querySelector('.menu__list');;
+const menuList = document.querySelector('.menu__list');
+
 if (iconMenu) {
 	// ОТКРЫТИЕ МЕНЮ-БУРГЕРА И АНИМАЦИЯ ЕГО ИКОНКИ
 	iconMenu.addEventListener('click', function (e) {
 		document.body.classList.toggle('_lock');
 		menuBody.classList.toggle('_active');
 		iconMenu.classList.toggle('_active');
+
 	});
 	// ЗАКТРЫТИЕ МЕНЮ ПРИ КЛИКЕ НА ЕГО ПОКРОВ ИЛИ ССЫЛКУ
 	menuList.addEventListener('click', function (e) {
